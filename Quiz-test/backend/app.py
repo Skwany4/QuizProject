2c6c2b14aa6e10291dc6f9feee5bfd2838b57e6f
Szymon Kwaśny <kwasny.szymon1@gmail.com>
2025-04-09 00:54:39 +0200

    add routes

--- Quiz-test/backend/app.py
+++ Quiz-test/backend/app.py
@@ -47,5 +47,16 @@ def login():
     else:
         return jsonify({"message": "Invalid credentials"}), 401
 
+
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    categories = list(db["categories"].find({}, {"_id": 0}))
+    return jsonify(categories), 200
+
+@app.route('/questions/<int:category_id>', methods=['GET'])
+def get_questions(category_id):
+    questions = list(db["questions"].find({"category_id": category_id}, {"_id": 0}))
+    return jsonify(questions), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
